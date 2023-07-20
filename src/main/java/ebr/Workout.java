96a8aeec65c0957f7d4e1931c667076ead7e6e28
lachrymaLF <timwtm@live.com>
2023-07-20 16:28:58 -0400

    Note that we need to split workout into two classes (another one for offerings that depend on workout types)

--- src/main/java/ebr/Workout.java
+++ src/main/java/ebr/Workout.java
@@ -1,4 +1,30 @@
 package ebr;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Workout {
+    public String name;
+    public Set<String> requiredCerts;
+
+    public Set<String> getRequiredCerts() {
+        return requiredCerts;
+    }
+
+    public boolean validateCerts(Set<String> certs) {
+        return certs.containsAll(requiredCerts);
+    }
+
+    public void requireCert(String cert) {
+        requiredCerts.add(cert);
+    }
+
+    public void deleteCert(String cert) {
+        requiredCerts.remove(cert);
+    }
+
+    public Workout(String name) {
+        this.name = name;
+        this.requiredCerts = new HashSet<>();
+    }
 }
