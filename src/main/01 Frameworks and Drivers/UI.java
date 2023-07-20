b5f8e1c29bdb30dd5fe15889bfc7b942005d4924
rishab <rishab_chopra@hotmail.com>
2023-07-20 14:31:36 -0400

    Pseudocode for the UI.java

---
+++ src/main/01 Frameworks and Drivers/UI.java
@@ -0,0 +1,17 @@
+// import statements
+
+//  UI class
+    // UI constructor
+
+    // default class (for login / entry purposes)
+        // default class constr.
+
+    // instructor ui class
+        // instructor ui class constr.
+
+    // admin ui class
+        // admin ui class constr.
+
+    // methods for interacting w/ Controller or Database go here
+
+    // public static void main(String[] args) { //entry point }
