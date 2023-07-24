77e12977635009208fc3d9b633c30b67a7dca783
Nick Chen <97940747+NickC64@users.noreply.github.com>
2023-07-24 18:17:27 -0400

    added attribute dependencies for Gym

--- src/main/java/ebr/Gym.java
+++ src/main/java/ebr/Gym.java
@@ -1,4 +1,11 @@
 package ebr;
 
+import java.util.List;
+
 public class Gym {
+    private Instructor instructors;
+    private List<Workout> classes;
+    private List<Room> rooms;
+    private Schedule schedule;
+
 }
