4be3c22b21f3f9a04d212a384ba0be0d6255c14d
rishab <rishab_chopra@hotmail.com>
2023-07-27 06:06:18 -0400

    Functional login, register, and authenticate screens, with functional login serialization and *encryption*. Code itself has yet to be cleaned up.

--- src/main/java/fd/RoundBtn.java
+++ src/main/java/fd/RoundBtn.java
@@ -4,8 +4,8 @@ package fd;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.RoundRectangle2D;
