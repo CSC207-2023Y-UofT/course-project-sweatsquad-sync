ce97ae9559833c7a554019c01328af6872b6c88b
lachrymaLF <timwtm@live.com>
2023-07-27 16:47:46 -0400

    fix hash&wkout test

--- src/test/java/ebr/EBRTest.java
+++ src/test/java/ebr/EBRTest.java
@@ -13,18 +13,19 @@ public class EBRTest {
         Room r1 = new Room("d");
         Room r2 = new Room("e");
         g.addRoom(r1);
-        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@0]");
+        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@64]");
         g.removeRooms(r2);
-        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@0]");
+        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@64]");
         g.addRoom(r2);
-        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@0, ebr.Room@1]");
+        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@64, ebr.Room@65]");
         g.removeRooms(r1);
-        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@1]");
+        Assertions.assertEquals(g.getRooms().toString(), "[ebr.Room@65]");
     }
     @Test
     public void WorkoutOfferingTest() {
         Workout w = new Workout("asdf");
-        w.offerings.add(new Workout.Offering(Workout.Weekday.Monday, LocalTime.of(10, 0), Duration.ofMinutes(30)));
+        Room r = new Room("r");
+        w.offerings.add(new Workout.Offering(Workout.Weekday.Monday, LocalTime.of(10, 0), Duration.ofMinutes(30), r));
         System.out.println(w.offerings);
     }
 }
