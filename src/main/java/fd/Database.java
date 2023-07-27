5c4b0c6d8af20b9ba9a42ad26d2577d692047705
rishab <rishab_chopra@hotmail.com>
2023-07-27 01:49:54 -0400

    Semi-complete login / register screens, with functional login serialization and *encryption*.

--- src/main/java/fd/Database.java
+++ src/main/java/fd/Database.java
@@ -1,4 +1,123 @@
 package fd;
 
+// import statements
+import java.util.*;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Database {
+
+    // declares database vars -hashmap, txt file, and hash toggle
+    private HashMap<String, String> users;
+    private final String filename = "user_credentials.txt";
+    private boolean useHashing = false; // can be toggled between T/F to hash
+
+    // database constructor
+    public Database() {
+        users = new HashMap<>(); // initializes the hashmap
+        loadUsers(); // loads existing users into hashmap
+    }
+
+
+    // method to load credentials from the "user_credentials.txt" file
+    private void loadUsers() {
+        try { // try-catch block to handle any IO  exceptions
+            File file = new File(filename); // creating a file object for u_c
+            if (file.exists()) { // checks if the file exists
+                Scanner reader = new Scanner(file); // creates scanner to read
+                while (reader.hasNextLine()) { // iterates through lines in file
+                    // splits line at the comma (between user and pass)
+                    String[] credentials = reader.nextLine().split(",");
+                    // adds username and passcode to the hashmap
+                    users.put(credentials[0], credentials[4]);
+                }
+                reader.close(); // closes scanner once done using
+            } else {
+                file.createNewFile(); // creates file if it doesn't exist
+            }
+        } catch (IOException e) { // catches any IO exception
+            e.printStackTrace(); // throws/prints error to console
+        }
+    }
+
+    // method to register new user, returns T if successful, F if already exists
+    public boolean register(String firstName, String lastName, String username,
+                            String email, String passcode) {
+        if (!validateInput(username) || !validateInput(passcode)) {
+            System.out.println("Invalid input");
+            return false;
+        } else if(users.containsKey(username)) {
+            System.out.println("Username already exists");
+            return false;
+        } else {
+            if (useHashing) {
+                passcode = hashPassword(passcode);
+            }
+            users.put(username, firstName + "," + lastName + "," +
+                    email + "," + passcode);
+            saveUsers();
+            return true;
+        }
+    }
+
+    // method to check if login credentials are valid
+    public boolean validateLogin(String username, String password) {
+        if(users.containsKey(username)) {
+            String storedPassword = users.get(username);
+            if (useHashing) { //checks for hash toggle
+                password = hashPassword(password);
+            }
+            if (storedPassword.equals(password)) {
+                System.out.println("Logged in successfully");
+                return true;
+            } else {
+                System.out.println("Incorrect password");
+                return false;
+            }
+        } else {
+            System.out.println("User does not exist");
+            return false;
+        }
+    }
+
+    // checks if input's valid
+    private boolean validateInput(String input) {
+        // returns T iff letters (both UC + LC), digits, and underscores, else F
+        return input.matches("\\w+");
+    }
+
+    // saves hashmap to the file
+    private void saveUsers() {
+        try { // to catch IO exceptions
+            FileWriter writer = new FileWriter(filename); // file-writer init
+
+            // loops through each entry in the hashmap
+            for (Map.Entry<String, String> entry : users.entrySet()) {
+                // writes each entry to the file, separated w/ comma
+                writer.write(entry.getKey() + "," + entry.getValue() + "\n");
+            }
+            writer.close(); // closes the FileWriter
+        } catch (IOException e) { // catches IO exception
+            e.printStackTrace(); // throws/prints stack trace
+        }
+    }
+
+    private String hashPassword(String password) {
+        try {
+            // inst of the SHA-256 messageDigest
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            // hashes password
+            byte[] hashedPassword = md.digest(password.getBytes());
+
+            StringBuilder sb = new StringBuilder(); // to create hash pw str
+            for (byte b : hashedPassword) { // loops through each bye
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return password;
+        }
+    }
 }
--- src/main/java/fd/UI.java
+++ src/main/java/fd/UI.java
@@ -1,116 +1,383 @@
 package fd;
 
+// import statements
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class UI extends JFrame implements ActionListener {
 
+    // declaring UI components / layout
     private CardLayout cardLayout = new CardLayout();
-    private JButton loginButton, signupButton, registerButton;
-    private JTextField usernameField, emailField;
-    private JPasswordField passwordField, confirmPasswordField;
-    private JPanel loginPanel, signupPanel;
+    private JButton loginButton, signupButton, registerButton, back2loginBtn;
+    private JButton returnLoginBtn, authenticateBtn;
+    private JTextField usernameField, userField, emailField, authField;
+    private JTextField firstNameField, lastNameField;
+    private JPasswordField passcodeField, passField, confirmPassField;
+    private JPanel loginPanel, signupPanel, authCodePanel;
+    private JLabel haveCodeLabel;
+    private RoundBtn roundBtn = new RoundBtn();
 
-    // Entry point of the program; creates instance of the UI class
+    // declaring the database instance (database.java)
+    private Database database;
+
+    // entry point of the program; creates instance of the UI class
     public static void main(String[] args) {
         new UI();
     }
 
+    // UI constructor - has a database, and covers login/registration
     public UI() {
-        setTitle("User Interface");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
-        setResizable(false);
-        setLayout(cardLayout);
 
-        // Define component width and height
-        int compWidth = 200;
-        int compHeight = 30;
+        //init Database instance
+        database = new Database();
 
-        // Calculate horizontal and vertical offset for centering
-        int xOffset = (getWidth() - compWidth) / 2;
-        int yOffset = (getHeight() - compHeight) / 2;
+        // ui 'defaults'
+        setTitle("SweatSquad Sync System"); // window title
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // ends program if x
+        setSize(800, 600); // window dimensions
+        setResizable(false); // disables resizing
+        setLayout(cardLayout); // layout form, switches between cards
+        setLocationRelativeTo(null); // centers ui if left 'null'
+
+        // define component width and height
+        int compW = 200; // dictates text field length
+        int compH = 30; // dictates text field height
+
+        // horizontal and vertical offset variable assignment for centering
+        int xOffset = (getWidth() - compW) / 2;
+        int spacing = 50; // define the space between the two columns
+        int xOffsetLeft = (getWidth() - 2 * compW - spacing) / 2;
+        int xOffsetRight = xOffsetLeft + compW + spacing;
+        int yOffset = (getHeight() - compH) / 2;
 
         // Login panel
         loginPanel = new JPanel();
         loginPanel.setLayout(null);
+        loginPanel.setBackground(Color.decode("#F5F4F2")); // pantone cool 25%
 
-        // Username field
-        usernameField = new JTextField(20);
-        usernameField.setBounds(xOffset, yOffset - 80, compWidth, compHeight);
+        // username label
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setFont(new Font("Monsterrat", Font.BOLD, 15));
+        usernameLabel.setBounds(189, 98, 422, 25);
+        loginPanel.add(usernameLabel);
+
+        // username field
+        usernameField = new JTextField(20); // 20 is default -negligible
+        usernameField.setBounds(189, 123, 422, 45);
+        usernameField.setBorder(new EmptyBorder(0, 20, 0, 20)); // removes bord.
+        usernameField.setFont(new Font("Comfortaa", Font.BOLD, 18));
         loginPanel.add(usernameField);
 
-        // Password field
-        passwordField = new JPasswordField(20);
-        passwordField.setBounds(xOffset, yOffset - 40, compWidth, compHeight);
-        loginPanel.add(passwordField);
+        // passcode label
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(new Font("Monsterrat", Font.BOLD, 15));
+        passwordLabel.setBounds(189, 185, 422, 25);
+        loginPanel.add(passwordLabel);
 
-        // Login button
-        loginButton = new JButton("Login");
-        loginButton.setBounds(xOffset, yOffset, 80, compHeight);
+        // passcode field
+        passcodeField = new JPasswordField(20); // 20 is default -negligible
+        passcodeField.setBounds(189, 210, 422, 45);
+        passcodeField.setBorder(new EmptyBorder(0, 20, 0, 20)); // removes bord.
+        loginPanel.add(passcodeField);
+
+        // login button (round button)
+        loginButton = roundBtn.genRoundBtn("Login", 50, "#001561", false);
+        loginButton.setFont(new Font("Monsterrat", Font.BOLD, 13));
+        loginButton.setForeground(Color.decode("#FFFFFF"));
+        loginButton.setBounds(189, 292, 196, 50);
         loginButton.addActionListener(this);
         loginPanel.add(loginButton);
 
-        // Signup button
-        signupButton = new JButton("Signup");
-        signupButton.setBounds(xOffset + 120, yOffset, 80, compHeight);
+        // signup button
+        signupButton = roundBtn.genRoundBtn("Create Account", 50, "#001561",
+                                            false);
+        signupButton.setFont(new Font("Monsterrat", Font.BOLD, 13));
+        signupButton.setForeground(Color.decode("#FFFFFF"));
+        signupButton.setBounds(413, 292, 196, 50);
         signupButton.addActionListener(this);
         loginPanel.add(signupButton);
 
-        // Signup panel
+        // login indicator bar JLabel
+        JLabel signIndicator = new JLabel("");
+        signIndicator.setIcon(new ImageIcon("images/00101-sign-indicator.png"));
+        signIndicator.setBounds(189, 65, 422, 9);
+        loginPanel.add(signIndicator);
+
+        // Sign In JLabel
+        JLabel signInTextLI = new JLabel("Sign In", SwingConstants.CENTER);
+        signInTextLI.setFont(new Font("Monsterrat", Font.BOLD, 16));
+        signInTextLI.setForeground(Color.decode("#001561"));
+        signInTextLI.setBounds(189, 25, 211, 47);
+        loginPanel.add(signInTextLI);
+
+        // Register JLabel
+        JLabel registerTextLI = new JLabel("Register", SwingConstants.CENTER);
+        registerTextLI.setFont(new Font("Monsterrat", Font.BOLD, 14));
+        registerTextLI.setForeground(Color.decode("#000000"));
+        registerTextLI.setBounds(400, 27, 211, 47);
+        loginPanel.add(registerTextLI);
+
+        // signup panel
         signupPanel = new JPanel();
         signupPanel.setLayout(null);
 
-        // Username field
-        usernameField = new JTextField(20);
-        usernameField.setBounds(xOffset, yOffset - 140, compWidth, compHeight);
-        signupPanel.add(usernameField);
+        /* first name label + field
+        JLabel firstNameLabel = new JLabel("First Name:");
+        firstNameLabel.setBounds(200, 145, 200, 30);
+        signupPanel.add(firstNameLabel);
 
-        // Email field
-        emailField = new JTextField(20);
-        emailField.setBounds(xOffset, yOffset - 100, compWidth, compHeight);
+        // first name field
+        firstNameField = new JTextField(20);
+        firstNameField.setBounds(200, 165, 200, 30);
+        signupPanel.add(firstNameField);*/
+
+        // first name label
+        JLabel firstNameLabel = new JLabel("First Name");
+        firstNameLabel.setFont(new Font("Monsterrat", Font.BOLD, 15));
+        firstNameLabel.setBounds(189, 98, 422, 25);
+        signupPanel.add(firstNameLabel);
+
+        // first name field
+        firstNameField = new JTextField(20); // 20 is default -negligible
+        firstNameField.setBounds(189, 123, 198, 45);
+        firstNameField.setBorder(new EmptyBorder(0, 20, 0, 20)); // removes bord.
+        firstNameField.setFont(new Font("Comfortaa", Font.BOLD, 18));
+        signupPanel.add(firstNameField);
+
+        // last name label
+        JLabel lastNameLabel = new JLabel("Last Name");
+        lastNameLabel.setFont(new Font("Monsterrat", Font.BOLD, 15));
+        lastNameLabel.setBounds(413, 98, 422, 25);
+        signupPanel.add(lastNameLabel);
+
+        // last name field
+        lastNameField = new JTextField(20); // 20 is default -negligible
+        lastNameField.setBounds(413, 123, 198, 45);
+        lastNameField.setBorder(new EmptyBorder(0, 20, 0, 20)); // removes bord.
+        lastNameField.setFont(new Font("Comfortaa", Font.BOLD, 18));
+        signupPanel.add(lastNameField);
+
+        // email label
+        JLabel emailLabel = new JLabel("Email:");
+        emailLabel.setFont(new Font("Monsterrat", Font.BOLD, 15));
+        emailLabel.setBounds(189, 185, 422, 25);
+        signupPanel.add(emailLabel);
+
+        // passcode field
+        emailField = new JTextField(20); // 20 is default -negligible
+        emailField.setBounds(189, 210, 422, 45);
+        emailField.setBorder(new EmptyBorder(0, 20, 0, 20)); // removes bord.
+        emailField.setFont(new Font("Comfortaa", Font.BOLD, 18));
         signupPanel.add(emailField);
 
-        // Password field
-        passwordField = new JPasswordField(20);
-        passwordField.setBounds(xOffset, yOffset - 60, compWidth, compHeight);
-        signupPanel.add(passwordField);
+        // username label + field
+        JLabel signupUsernameLabel = new JLabel("Username");
+        signupUsernameLabel.setBounds(500, 145, 200, 30);
+        signupPanel.add(signupUsernameLabel);
+        userField = new JTextField(20);
+        userField.setBounds(500, 165, 200, 30);
+        signupPanel.add(userField);
 
-        // Confirm password field
-        confirmPasswordField = new JPasswordField(20);
-        confirmPasswordField.setBounds(xOffset, yOffset - 20, compWidth, compHeight);
-        signupPanel.add(confirmPasswordField);
+        // passcode label + field
+        JLabel signupPasswordLabel = new JLabel("Password");
+        signupPasswordLabel.setBounds(500, 205, 200, 30);
+        signupPanel.add(signupPasswordLabel);
+        passField = new JPasswordField(20);
+        passField.setBounds(500, 225, 200, 30);
+        signupPanel.add(passField);
 
-        // Register button
+        // confirm passcode label + field
+        JLabel confirmPasswordLabel = new JLabel("Confirm Password:");
+        confirmPasswordLabel.setBounds(500, 265, 200, 30);
+        signupPanel.add(confirmPasswordLabel);
+        confirmPassField = new JPasswordField(20);
+        confirmPassField.setBounds(500, 285, 200, 30);
+        signupPanel.add(confirmPassField);
+
+        // register button
         registerButton = new JButton("Register");
-        registerButton.setBounds(xOffset, yOffset + 20, 100, compHeight);
+        registerButton.setBounds(420, 345, 100, 30);
         registerButton.addActionListener(this);
         signupPanel.add(registerButton);
 
-        add(loginPanel, "Login");
-        add(signupPanel, "Signup");
+        // back button
+        back2loginBtn = new JButton("Back");
+        back2loginBtn.setBounds(280, 345, 100, 30);
+        back2loginBtn.addActionListener(this);
+        signupPanel.add(back2loginBtn);
+
+        haveCodeLabel = new JLabel("<HTML><U>I have a code</U></HTML>");
+        haveCodeLabel.setForeground(Color.BLUE);
+        haveCodeLabel.setBounds(350, 385, 200, 30);
+        haveCodeLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        haveCodeLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // clear all the fields
+                usernameField.setText("");
+                passcodeField.setText("");
+                userField.setText("");
+                emailField.setText("");
+                passField.setText("");
+                confirmPassField.setText("");
+
+                // switch the card layout back to the login panel
+                cardLayout.show(getContentPane(), "AuthCode");
+            }
+        });
+        signupPanel.add(haveCodeLabel);
+
+        /* login panel background
+        JLabel signupBG = new JLabel("");
+        signupBG.setIcon(new ImageIcon("images/signupBG.png"));
+        signupBG.setBounds(0, -10, 800, 600);
+        signupPanel.add(signupBG);*/
+
+        // login indicator bar JLabel
+        JLabel regIndicator = new JLabel("");
+        regIndicator.setIcon(new ImageIcon("images/00201-reg-indicator.png"));
+        regIndicator.setBounds(189, 65, 422, 9);
+        signupPanel.add(regIndicator);
+
+        // Sign In JLabel
+        JLabel signInTextSU = new JLabel("Sign In", SwingConstants.CENTER);
+        signInTextSU.setFont(new Font("Monsterrat", Font.BOLD, 14));
+        signInTextSU.setForeground(Color.decode("#000000"));
+        signInTextSU.setBounds(189, 27, 211, 47);
+        signupPanel.add(signInTextSU);
+
+        // Register JLabel
+        JLabel registerTextSU = new JLabel("Register", SwingConstants.CENTER);
+        registerTextSU.setFont(new Font("Monsterrat", Font.BOLD, 16));
+        registerTextSU.setForeground(Color.decode("#001561"));
+        registerTextSU.setBounds(400, 25, 211, 47);
+        signupPanel.add(registerTextSU);
+
+        // instructor authenticate code panel
+        authCodePanel = new JPanel();
+        authCodePanel.setLayout(null);
+
+        // auth code label + field
+        JLabel authCodeLabel = new JLabel("Authorization Code:");
+        authCodeLabel.setBounds(300, 225, 200, 30);
+        authCodePanel.add(authCodeLabel);
+        authField = new JTextField(20);
+        authField.setBounds(300, 245, 200, 30);
+        authCodePanel.add(authField);
+
+        // return to log-in btn
+        returnLoginBtn = new JButton("Return");
+        returnLoginBtn.setBounds(295, 285, 95, 30);
+        returnLoginBtn.addActionListener(this);
+        authCodePanel.add(returnLoginBtn);
+
+        // authenticate btn
+        authenticateBtn = new JButton("Authenticate");
+        authenticateBtn.setBounds(395, 285, 110, 30);
+        authenticateBtn.addActionListener(this);
+        authCodePanel.add(authenticateBtn);
+
+        add(loginPanel, "Login"); // adds panel to the card "deck"
+        add(signupPanel, "Signup");  // adds panel to the card "deck"
+        add(authCodePanel, "AuthCode");  // adds panel to the card "deck"
 
         setVisible(true);
     }
 
+    // button action listener response function
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == loginButton) {
-            // login logic is missing
-            System.out.println("doesn't work yet lol");
-        } else if (e.getSource() == signupButton) {
-            cardLayout.show(getContentPane(), "Signup");
-        } else if (e.getSource() == registerButton) {
+        if (e.getSource() == loginButton) { // checks 4 loginButton; login logic
+
+            // fetches username + passcode from JTextField components
             String username = usernameField.getText();
+            String password = new String(passcodeField.getPassword());
+
+            // checks if credentials are correct; uses Database.validateLogin
+            if(database.validateLogin(username, password)) {
+                System.out.println("Logged in successfully"); // console msg
+            } else {
+                System.out.println("Invalid credentials"); // failed console msg
+            }
+
+        } else if (e.getSource() == signupButton) { // signup logic if pressed
+            cardLayout.show(getContentPane(), "Signup"); // switches to SU panel
+            // resets fields (signup screen)
+            userField.setText("");
+            emailField.setText("");
+            passField.setText("");
+            confirmPassField.setText("");
+
+        } else if (e.getSource() == registerButton) { // register logic
+            // gets text from respective JTextField components
+            String firstName = firstNameField.getText();
+            String lastName = lastNameField.getText();
+            String username = userField.getText();
             String email = emailField.getText();
-            String password = new String(passwordField.getPassword());
-            String confirmPassword = new String(confirmPasswordField.getPassword());
+            String password = new String(passField.getPassword());
+            String confirmPassword = new String(confirmPassField.getPassword());
 
-            System.out.println("Username: " + username);
-            System.out.println("Email: " + email);
-            System.out.println("Password: " + password);
-            System.out.println("Confirm Password: " + confirmPassword);
+            if (username.length() < 3) {
+                System.out.println("Username must be at least " +
+                        "3 characters long");
+                return;
+            }
 
+            if (firstName.isEmpty() || lastName.isEmpty()) {
+                System.out.println("First and last name fields " +
+                        "must not be empty");
+                return;
+            }
+
+            if (!email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+                System.out.println("Invalid email format");
+                return;
+            }
+
+            if (!password.equals(confirmPassword)) {
+                System.out.println("Passwords do not match, try again");
+                return;
+            }
+
+            if(database.register(firstName, lastName, username,
+                    email, password)) { // checks for uniqueness
+                System.out.println("Registered successfully"); // console msg
+
+                // console messages for testing
+                System.out.println("Username: " + username);
+                System.out.println("Email: " + email);
+                System.out.println("Password: " + password);
+                System.out.println("Confirm Password: " + confirmPassword);
+
+                // Switch the card layout back to the login panel + reset JTFs
+                usernameField.setText("");
+                passcodeField.setText("");
+                cardLayout.show(getContentPane(), "Login");
+            } else {
+                System.out.println("Username already exists"); // F console msg
+            }
+        } else if (e.getSource() == back2loginBtn) { // if back button is clicked
+            // clears all the fields
+            usernameField.setText("");
+            passcodeField.setText("");
+            userField.setText("");
+            emailField.setText("");
+            passField.setText("");
+            confirmPassField.setText("");
+
+            // switches the card layout back to log-in panel
+            cardLayout.show(getContentPane(), "Login");
+        } else if (e.getSource() == returnLoginBtn) {
+            // clears all the fields
+            usernameField.setText("");
+            passcodeField.setText("");
+            userField.setText("");
+            emailField.setText("");
+            passField.setText("");
+            confirmPassField.setText("");
+
+            // switches the card layout back to log-in panel
             cardLayout.show(getContentPane(), "Login");
         }
     }
