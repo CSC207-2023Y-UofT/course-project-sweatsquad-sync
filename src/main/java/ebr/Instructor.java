9dca58cea3d05612f121416a6f8a659552bd308b
lachrymaLF <timwtm@live.com>
2023-07-27 17:16:19 -0400

    proper instructor & certs

--- src/main/java/ebr/Instructor.java
+++ src/main/java/ebr/Instructor.java
@@ -1,13 +1,17 @@
 package ebr;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Instructor extends RegisteredUser implements Serializable {
     protected Instructor() {};
     // N.B. Set code below to null when instructor account is claimed
     public String tempAuth;
+    public Set<String> certs;
     public Instructor(String name, String password) {
         super(name, password);
         tempAuth = "1234567890";
+        certs = new HashSet<>();
     }
 }
