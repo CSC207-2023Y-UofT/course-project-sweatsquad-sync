690f14450eb302570c3f7189dbf23422e27c68b0
rishab <rishab_chopra@hotmail.com>
2023-08-03 11:00:37 -0400

    Addition of User Screens
    Screens for unregistered users have been connected to the dashboard and can be accessed upon logging in; they allow for traversal across all possible screens the unregistered user should have access to.
    
    Issues / Fixes:
    Component positioning needs to be modified to better use the space.
    
    Course

--- src/main/java/fd/UI.java
+++ src/main/java/fd/UI.java
@@ -21,10 +21,17 @@ public class UI extends JFrame implements ActionListener {
     private JPasswordField passcodeField, passField, confirmPassField;
     private JPasswordField passFieldIR, confirmPassFieldIR;
     private JPanel loginPanel, signupPanel, authCodePanel, instrRegPanel, mainP;
-    private JPanel schedulePanel, enrolPanel, smthPanel, settingsPanel;
+    private JPanel schedulePanel, enrolPanel, smthPanel;
+    private JPanel userDash, regUserDash, instructorDash, adminDash;
     private JLabel haveCodeLabel, welcomeUser, err1, err2, err3, err4, viewType;
+    private JLabel dashboardTitle;
+    private JButton viewClassOfferings, dailyWorkoutTips, accountSettings;
+    private JPanel viewOfferingsPanel, workoutTipsPanel, settingsPanel;
+    private JButton back2dash;
     private RoundBtn roundBtn = new RoundBtn();
     private RoundField roundField = new RoundField();
+
+    public String activeDash;
 
     // declaring the database instance (database.java)
     private Database database;
@@ -490,23 +497,86 @@ public class UI extends JFrame implements ActionListener {
         mainP.setLayout(null);
         mainP.setBackground(Color.decode("#DADADA"));
 
-        JLabel dashboardTitle = new JLabel("Dashboard");
+        // main logged-in view panel
+        userDash = new JPanel();
+        userDash.setLayout(null);
+        userDash.setBackground(Color.decode("#DADADA"));
+
+        dashboardTitle = new JLabel("Dashboard");
         dashboardTitle.setFont(new Font("Monsterrat", Font.BOLD, 28));
         dashboardTitle.setForeground(Color.decode("#001561"));
         dashboardTitle.setBounds(64, 25, 422, 47);
-        mainP.add(dashboardTitle);
+        userDash.add(dashboardTitle);
 
         welcomeUser = new JLabel("");
         welcomeUser.setFont(new Font("Monsterrat", Font.BOLD, 20));
         welcomeUser.setForeground(Color.decode("#001561"));
         welcomeUser.setBounds(66, 64, 422, 47);
-        mainP.add(welcomeUser);
+        userDash.add(welcomeUser);
+
+        logout = roundBtn.genRoundBtn("⏻", 30, "#001561", false);
+        logout.setFont(new Font("Monsterrat", Font.BOLD, 19));
+        logout.setForeground(Color.decode("#FFFFFF"));
+        logout.setBounds(608, 10, 48, 30);
+        logout.addActionListener(this);
+        userDash.add(logout);
+
+        viewType = new JLabel("");
+        viewType.setFont(new Font("Monsterrat", Font.BOLD, 20));
+        viewType.setForeground(Color.decode("#001561"));
+        viewType.setBounds(670, 16, 150, 26);
+        userDash.add(viewType);
+
+        viewClassOfferings = roundBtn.genRoundBtn("View Class Offerings", 30, "#001561", false);
+        viewClassOfferings.setFont(new Font("Monsterrat", Font.BOLD, 20));
+        viewClassOfferings.setForeground(Color.decode("#FFFFFF"));
+        viewClassOfferings.setBounds(174, 130, 452, 60);
+        viewClassOfferings.addActionListener(this);
+        userDash.add(viewClassOfferings);
+
+        viewOfferingsPanel = new JPanel();
+        viewOfferingsPanel.setLayout(null);
+        viewOfferingsPanel.setBackground(Color.decode("#DADADA"));
+
+        back2dash = roundBtn.genRoundBtn("Back", 30, "#001561", false);
+        back2dash.setFont(new Font("Monsterrat", Font.BOLD, 20));
+        back2dash.setForeground(Color.decode("#FFFFFF"));
+        back2dash.setBounds(592, 478, 138, 68);
+        back2dash.addActionListener(this);
+        viewOfferingsPanel.add(back2dash);
+
+        dailyWorkoutTips = roundBtn.genRoundBtn("Daily Workout Tips", 30, "#001561", false);
+        dailyWorkoutTips.setFont(new Font("Monsterrat", Font.BOLD, 20));
+        dailyWorkoutTips.setForeground(Color.decode("#FFFFFF"));
+        dailyWorkoutTips.setBounds(174, 230, 452, 60);
+        dailyWorkoutTips.addActionListener(this);
+        userDash.add(dailyWorkoutTips);
+
+        workoutTipsPanel = new JPanel();
+        workoutTipsPanel.setLayout(null);
+        workoutTipsPanel.setBackground(Color.decode("#DADADA"));
+
+        settings = roundBtn.genRoundBtn("Account Settings ⚙", 30, "#001561", false);
+        settings.setFont(new Font("Monsterrat", Font.BOLD, 20));
+        settings.setForeground(Color.decode("#FFFFFF"));
+        settings.setBounds(174, 330, 452, 60);
+        settings.addActionListener(this);
+        userDash.add(settings);
+
+        settingsPanel = new JPanel();
+        settingsPanel.setLayout(null);
+        settingsPanel.setBackground(Color.decode("#DADADA"));
+
+        // main logged-in view panel
+        instructorDash = new JPanel();
+        instructorDash.setLayout(null);
+        instructorDash.setBackground(Color.decode("#DADADA"));
 
         JLabel upcomingC = new JLabel("Upcoming Classes");
         upcomingC.setFont(new Font("Monsterrat", Font.BOLD, 20));
         upcomingC.setForeground(Color.decode("#000000"));
         upcomingC.setBounds(66, 130, 422, 47);
-        mainP.add(upcomingC);
+        instructorDash.add(upcomingC);
 
         JLabel upcomingC1 = new JLabel("Class 1 Info");
         upcomingC1.setFont(new Font("Monsterrat", Font.BOLD, 20));
@@ -514,7 +584,7 @@ public class UI extends JFrame implements ActionListener {
         upcomingC1.setBackground(Color.decode("#40508a"));
         upcomingC1.setOpaque(true);
         upcomingC1.setBounds(66, 180, 320, 60);
-        mainP.add(upcomingC1);
+        instructorDash.add(upcomingC1);
 
         JLabel upcomingC2 = new JLabel("Class 2 Info");
         upcomingC2.setFont(new Font("Monsterrat", Font.BOLD, 20));
@@ -523,7 +593,7 @@ public class UI extends JFrame implements ActionListener {
         upcomingC2.setOpaque(true);
         upcomingC2.setBounds(66, 275, 320, 60);
         upcomingC2.setOpaque(true);
-        mainP.add(upcomingC2);
+        instructorDash.add(upcomingC2);
 
         JLabel upcomingC3 = new JLabel("Class 3 Info");
         upcomingC3.setFont(new Font("Monsterrat", Font.BOLD, 20));
@@ -531,7 +601,7 @@ public class UI extends JFrame implements ActionListener {
         upcomingC3.setBackground(Color.decode("#40508a"));
         upcomingC3.setOpaque(true);
         upcomingC3.setBounds(66, 370, 320, 60);
-        mainP.add(upcomingC3);
+        instructorDash.add(upcomingC3);
 
         viewEntireSchedule = roundBtn.genRoundBtn("View Entire Schedule", 30,
                                                   "#001561", false);
@@ -539,47 +609,14 @@ public class UI extends JFrame implements ActionListener {
         viewEntireSchedule.setForeground(Color.decode("#FFFFFF"));
         viewEntireSchedule.setBounds(66, 445, 320, 40);
         viewEntireSchedule.addActionListener(this);
-        mainP.add(viewEntireSchedule);
+        instructorDash.add(viewEntireSchedule);
 
-        settings = roundBtn.genRoundBtn("⚙", 30, "#001561", false);
-        settings.setFont(new Font("Monsterrat", Font.BOLD, 24));
-        settings.setForeground(Color.decode("#FFFFFF"));
-        settings.setBounds(608, 12, 48, 32);
-        settings.addActionListener(this);
-        mainP.add(settings);
-
-        viewType = new JLabel("User View");
-        viewType.setFont(new Font("Monsterrat", Font.BOLD, 20));
-        viewType.setForeground(Color.decode("#001561"));
-        viewType.setBounds(670, 16, 150, 26);
-        mainP.add(viewType);
-
-        enrolBtn = roundBtn.genRoundBtn("Enroll in Classes", 30, "#001561",
-                                        false);
+        enrolBtn = roundBtn.genRoundBtn("Manage Classes", 30, "#001561", false);
         enrolBtn.setFont(new Font("Monsterrat", Font.BOLD, 23));
         enrolBtn.setForeground(Color.decode("#FFFFFF"));
-        enrolBtn.setBounds(490, 180, 218, 100);
+        enrolBtn.setBounds(490, 180, 218, 75);
         enrolBtn.addActionListener(this);
-        mainP.add(enrolBtn);
-
-        smthBtn = roundBtn.genRoundBtn("Smth", 30, "#001561", false);
-        smthBtn.setFont(new Font("Monsterrat", Font.BOLD, 23));
-        smthBtn.setForeground(Color.decode("#FFFFFF"));
-        smthBtn.setBounds(490, 300, 218, 100);
-        smthBtn.addActionListener(this);
-        mainP.add(smthBtn);
-
-        logout = roundBtn.genRoundBtn("Logout", 30, "#001561", false);
-        logout.setFont(new Font("Monsterrat", Font.BOLD, 23));
-        logout.setForeground(Color.decode("#FFFFFF"));
-        logout.setBounds(490, 420, 218, 64);
-        logout.addActionListener(this);
-        mainP.add(logout);
-
-        // settings panel
-        settingsPanel = new JPanel();
-        settingsPanel.setLayout(null);
-        settingsPanel.setBackground(Color.decode("#DADADA"));
+        instructorDash.add(enrolBtn);
 
         // Login panel
         schedulePanel = new JPanel();
@@ -600,8 +637,13 @@ public class UI extends JFrame implements ActionListener {
         add(signupPanel, "Signup"); // adds panel to the card "deck"
         add(authCodePanel, "AuthCode"); // adds panel to the card "deck"
         add(instrRegPanel, "InstrReg"); // adds panel to the card "deck"
-        add(mainP, "main"); // adds panel to the card "deck"
-        add(settingsPanel, "settings"); // adds panel to the card "deck"
+        add(userDash, "User Dashboard"); // adds panel to the card "deck"
+        add(viewOfferingsPanel, "Offerings"); // adds panel to the card "deck"
+        add(workoutTipsPanel, "Daily Workout Tips"); // adds panel to card deck
+        add(instructorDash, "Instructor Dashboard"); // adds panel to card deck
+        add(settingsPanel, "Account Settings"); // adds panel to the card "deck"
+
+        add(mainP, "main"); // adds panel to the card "deck" todo deprecated
         add(schedulePanel, "schedule"); // adds panel to the card "deck"
         add(enrolPanel, "enrolment"); // adds panel to the card "deck"
         add(smthPanel, "something"); // adds panel to the card "deck"
@@ -626,12 +668,32 @@ public class UI extends JFrame implements ActionListener {
                                     database.activeUser.firstName + "!");
                 if (database.activeUser instanceof Instructor) {
                     viewType.setText("Instructor View");
+                    instructorDash.add(viewType);
+                    instructorDash.add(welcomeUser);
+                    instructorDash.add(dashboardTitle);
+                    instructorDash.add(logout);
+                    instructorDash.add(dailyWorkoutTips);
+                    dailyWorkoutTips.setBounds(490, 295, 218, 75);
+                    instructorDash.add(settings);
+                    settings.setBounds(490, 410, 218, 75);
+                    activeDash = "Instructor Dashboard";
                 } else if (database.activeUser instanceof GymAdmin) {
                     viewType.setText("Admin View");
+                    activeDash = "Admin Dashboard";
                 } else {
                     viewType.setText("User View");
+                    userDash.add(viewType);
+                    userDash.add(welcomeUser);
+                    userDash.add(dashboardTitle);
+                    userDash.add(logout);
+                    userDash.add(dailyWorkoutTips);
+                    dailyWorkoutTips.setBounds(174, 230, 452, 60);
+                    userDash.add(settings);
+                    settings.setBounds(174, 330, 452, 60);
+                    activeDash = "User Dashboard";
                 }
-                cardLayout.show(getContentPane(), "main");
+                cardLayout.show(getContentPane(), activeDash);
+
             } else {
                 System.out.println("Invalid credentials"); // failed console msg
             }
@@ -649,8 +711,8 @@ public class UI extends JFrame implements ActionListener {
 
         } else if (e.getSource() == registerButton) { // register logic
             // gets text from respective JTextField components
-            String firstName = firstNameField.getText();
-            String lastName = lastNameField.getText();
+            String firstName = capitalizeRemoveTrailingSpaces(firstNameField.getText());
+            String lastName = capitalizeRemoveTrailingSpaces(lastNameField.getText());
             String username = userField.getText();
             String email = emailField.getText();
             String password = new String(passField.getPassword());
@@ -797,6 +859,8 @@ public class UI extends JFrame implements ActionListener {
                 return;
             }
 
+
+
             if (database.claimInstructor(database.validateAuthCode(authField.getText()), firstName, lastName, username,
                     email, password)) { // uniqueness
                 authField.setText("");
@@ -816,17 +880,35 @@ public class UI extends JFrame implements ActionListener {
                 err3.setText("<HTML>*Username already<BR>exists<HTML>");
                 System.out.println("Username already exists"); // F console msg
             }
-        } else if (e.getSource() == viewEntireSchedule) {
-            cardLayout.show(getContentPane(), "schedule");
-        } else if (e.getSource() == enrolBtn) {
-            cardLayout.show(getContentPane(), "enrolment");
+        } else if (e.getSource() == viewClassOfferings) {
+            viewOfferingsPanel.add(back2dash);
+            cardLayout.show(getContentPane(), "Offerings");
+        } else if (e.getSource() == dailyWorkoutTips) {
+            workoutTipsPanel.add(back2dash);
+            cardLayout.show(getContentPane(), "Daily Workout Tips");
+        } else if (e.getSource() == settings) {
+            settingsPanel.add(back2dash);
+            cardLayout.show(getContentPane(), "Account Settings");
         } else if (e.getSource() == smthBtn) {
             cardLayout.show(getContentPane(), "something");
-        } else if (e.getSource() == settings) {
-            cardLayout.show(getContentPane(), "settings");
         } else if (e.getSource() == logout) {
+            database.activeUser = null;
             System.out.println("Logged out successfully");
             cardLayout.show(getContentPane(), "Login");
+        } else if (e.getSource() == back2dash) {
+            cardLayout.show(getContentPane(), activeDash);
+        }
+    }
+
+    // removes trailing spaces from the front -used for names
+    public static String capitalizeRemoveTrailingSpaces(String input) {
+        String trimmedInput = input.trim(); // removes whitespace on both ends
+
+        // capitalizes the first letter
+        if (!trimmedInput.isEmpty()) { // checks that there is a first letter
+            return Character.toUpperCase(trimmedInput.charAt(0)) + trimmedInput.substring(1);
+        } else {
+            return ""; // no letters = empty string
         }
     }
 }
