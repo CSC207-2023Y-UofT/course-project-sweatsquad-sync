c45182312a79d0020d45f0302bd215a0bdf95bfa
lachrymaLF <timwtm@live.com>
2023-08-02 22:18:53 -0400

    GymAdmin constructor

--- src/main/java/ebr/GymAdmin.java
+++ src/main/java/ebr/GymAdmin.java
@@ -1,4 +1,8 @@
 package ebr;
 
+
 public class GymAdmin extends RegisteredUser {
+    public GymAdmin(String name, String password, String firstName, String lastName, String email) {
+        super(name, password, firstName, lastName, email);
+    }
 }
