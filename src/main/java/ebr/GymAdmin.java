498443ab925fb5b1c9f2769f3b0d129b1b1ba4aa
rishab <rishab_chopra@hotmail.com>
2023-08-03 19:46:54 -0400

    Hotfix for previous rebase overwriting changes in GymAdmin.java

--- src/main/java/ebr/GymAdmin.java
+++ src/main/java/ebr/GymAdmin.java
@@ -1,7 +1,7 @@
 package ebr;
 
 
-public class GymAdmin extends RegisteredUser {
+public class GymAdmin extends User {
     public GymAdmin(String name, String password, String firstName, String lastName, String email) {
         super(name, password, firstName, lastName, email);
     }
