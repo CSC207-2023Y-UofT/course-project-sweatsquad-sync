e8263f40fcfdfa2af789742f9595a59350524154
lachrymaL <lachrymalfutura@gmail.com>
2023-08-04 16:37:46 -0400

    Force selection models on Schedule/Course tables.

--- src/main/java/fd/CourseFrame.java
+++ src/main/java/fd/CourseFrame.java
@@ -1,6 +1,8 @@
 package fd;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,7 +12,7 @@ public class CourseFrame extends JDialog implements ActionListener {
     private JButton addCourse, editCourse;
     private AbstractTableModel courseTable = new AbstractTableModel() {
         private final String[] enrolTableCols = {"Workout", "Room", "Time", "Instructor", "Status"};
-        public int getColumnCount() { return 5; }
+        public int getColumnCount() { return enrolTableCols.length; }
         public int getRowCount() { return (int)App.db.getCurrentWorkouts().size(); }
         public String getColumnName(int col) {
             return enrolTableCols[col];
@@ -36,11 +38,21 @@ public class CourseFrame extends JDialog implements ActionListener {
         editCourse = new JButton("edit");
         editCourse.setBounds(0, 0, 100, 40);
         editCourse.addActionListener(this);
+        editCourse.setEnabled(false);
         this.add(editCourse);
 
         JTable enrolTable = new JTable(courseTable);
         enrolTable.getTableHeader().setResizingAllowed(false);
         enrolTable.getTableHeader().setReorderingAllowed(false);
+        enrolTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        enrolTable.setRowSelectionAllowed(true);
+        enrolTable.setColumnSelectionAllowed(false);
+        enrolTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                editCourse.setEnabled(enrolTable.getSelectedRow() != -1);
+            }
+        });
         JScrollPane p = new JScrollPane(enrolTable);
         p.setBounds(0, 40, 800, 500);
         this.add(p);
@@ -62,7 +74,7 @@ public class CourseFrame extends JDialog implements ActionListener {
                         courseTable.fireTableDataChanged();
                     }
                     catch (NumberFormatException exp) {
-                        JOptionPane.showMessageDialog(this, "Invalid capacity!");
+                        JOptionPane.showMessageDialog(this, "Workout already exists or invalid capacity!");
                     }
                 }
             }
