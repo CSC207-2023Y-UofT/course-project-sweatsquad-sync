0ec17c3addbe7f0180ebbc6c9d7d48fcc8d6aee4
rishab <rishab_chopra@hotmail.com>
2023-08-06 17:48:58 -0400

    Setup for User Profile Changes
    Introduced the tab and provided starter code to set up for further development/functionality.
    
    Issue:
    Indicator (underline) isn't working as intended.

--- src/main/java/fd/UserInfoFrame.java
+++ src/main/java/fd/UserInfoFrame.java
@@ -1,96 +1,102 @@
 package fd;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class UserInfoFrame extends JDialog implements ActionListener  {
+public class UserInfoFrame extends JFrame implements ActionListener {
 
+    private JButton infoChange, passChange;
+    private JLabel underLine;
+    private CardLayout cardLayout = new CardLayout();
+    private JPanel cards = new JPanel(cardLayout);
+    private JPanel detailsPanel = new JPanel();
+    private JPanel passChangePanel = new JPanel();
 
     public UserInfoFrame() {
-        setTitle("Edit User Information"); // window title
-        setSize(800, 600); // window dimensions
-        setResizable(false); // disables resizing
-        setLocationRelativeTo(null); // centers ui if left 'null'
+        setTitle("Edit User Information");
+        setSize(800, 600);
+        setResizable(false);
+        setLocationRelativeTo(null);
         setLayout(null);
-        setModal(true);
-        // TODO actually i think you may be able to just spawn a SignupPanel and add it to this frame
-        //  (may need to change some stuff from there though (but still better than redoing this))
-//        firstNameField.setText(database.activeUser.firstName); // set FN
-//        settingsPanel.add(lastNameLabel);
-//        settingsPanel.add(lastNameField);
-//        lastNameField.setText(database.activeUser.lastName); // set LN
-//        settingsPanel.add(emailLabel);
-//        settingsPanel.add(emailField);
-//        emailField.setText(database.activeUser.email); // set email
-//        settingsPanel.add(signupUsernameLabel);
-//        settingsPanel.add(userField);
-//        userField.setText(database.activeUser.getName());
-//        settingsPanel.add(err1);
-//        settingsPanel.add(err2);
-//        settingsPanel.add(err3);
-//        settingsPanel.add(err4);
-//        settingsPanel.add(back2dash);
-//        back2dash.setBounds(12, 12, 138, 68);
-//        cardLayout.show(getContentPane(), "Account Settings");
-        //        saveAccountChanges = UI.genRoundBtn("Save Changes", 50, "#001561", false);
-//        saveAccountChanges.setFont(UI.MB13);
-//        saveAccountChanges.setForeground(Color.decode("#FFFFFF"));
-//        saveAccountChanges.setBounds(189, 450, 422, 50);
-//        saveAccountChanges.addActionListener(this);
-//        this.add(saveAccountChanges);
+
+        infoChange = new JButton("Account Details"); // left tab
+        infoChange.setBounds(180, 27, 225, 50);
+        infoChange.addActionListener(this);
+        infoChange.setOpaque(false);
+        infoChange.setContentAreaFilled(false);
+        infoChange.setBorderPainted(false);
+        infoChange.setFocusPainted(false);
+        add(infoChange);
+
+        passChange = new JButton("Change Password"); // right tab
+        passChange.setBounds(395, 27, 225, 50);
+        passChange.addActionListener(this);
+        passChange.setOpaque(false);
+        passChange.setContentAreaFilled(false);
+        passChange.setBorderPainted(false);
+        passChange.setFocusPainted(false);
+        add(passChange);
+
+        underLine = new JLabel(""); // underline
+        underLine.setBounds(189, 65, 422, 9);
+        add(underLine);
+
+        // content
+        // detailsPanel.add(new JLabel("Details content goes here"));
+        // passChangePanel.add(new JLabel("Password change content goes here"));
+
+        // adding panels to cards
+        cards.add(detailsPanel, "Details");
+        cards.add(passChangePanel, "PassChange");
+        cards.setBounds(180, 100, 600, 450);
+        add(cards);
+
+        infoChangeCard();  // default card (left tab)
     }
 
+    public void infoChangeCard() {
+        cardLayout.show(cards, "Details");
 
-//    // button action listener response function
-//    public void actionPerformed(ActionEvent e) {
-//        if (e.getSource() == saveAccountChanges) {
-//            String firstName = capitalizeRemoveTrailingSpaces(firstNameField.getText());
-//            firstNameField.setText(firstName);
-//            String lastName = capitalizeRemoveTrailingSpaces(lastNameField.getText());
-//            lastNameField.setText(lastName);
-//            String username = userField.getText().trim();
-//            String email = emailField.getText().trim();
-//            emailField.setText(email);
-//            err1.setText("");
-//            err2.setText("");
-//            err3.setText("");
-//            err4.setText("");
-//
-//            if (!firstName.equals(database.activeUser.firstName)) {
-//                if (!firstName.isEmpty()) {
-//                    database.activeUser.firstName = firstName;
-//                } else {
-//                    System.out.println("Name fields cannot be left blank");
-//                }
-//            }
-//
-//            if (!lastName.equals(database.activeUser.lastName)) {
-//                if (!lastName.isEmpty()) {
-//                    database.activeUser.lastName = lastName;
-//                } else {
-//                    System.out.println("Name fields cannot be left blank");
-//                }
-//            }
-//
-//            if (!email.equals(database.activeUser.email)) {
-//                if (email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
-//                    database.activeUser.email = email;
-//                } else {
-//                    System.out.println("Invalid email format.");
-//                }
-//            }
-//
-//            if (!username.equals(database.activeUser.firstName)) {
-//                // TODO
-//            }
-//        }
-//    }
+        infoChange.setFocusable(false);
+        infoChange.setFont(UI.MB16);  // Placeholder font
+        infoChange.setForeground(Color.decode("#001561"));
+
+        passChange.setFocusable(true);
+        passChange.setFont(UI.MB14);  // Placeholder font
+        passChange.setForeground(Color.decode("#000000"));
+
+        underLine.setIcon(new ImageIcon("images/00101-sign-indicator.png"));
+    }
+
+    public void passChangeCard() {
+        cardLayout.show(cards, "PassChange");
+
+        infoChange.setFocusable(true);
+        infoChange.setFont(UI.MB14);
+        infoChange.setForeground(Color.decode("#000000"));
+
+        passChange.setFocusable(false);
+        passChange.setFont(UI.MB16);
+        passChange.setForeground(Color.decode("#001561"));
+
+        underLine.setIcon(new ImageIcon("images/00101-sign-indicator.png"));
+    }
+
     public void refreshShow() {
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == infoChange)
+            infoChangeCard();
+        else if (e.getSource() == passChange)
+            passChangeCard();
+    }
+
+    public static void main(String[] args) {  // Just for testing
+        new UserInfoFrame();
     }
 }
