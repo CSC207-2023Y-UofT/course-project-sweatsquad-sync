b73ba615b92fc04ea3bc4faea064848c8d4fd1e6
rishab <rishab_chopra@hotmail.com>
2023-08-06 18:02:22 -0400

    Custom JTextFields Added (Profile Update)
    Round JTextFields and their JLabel counterparts have been added to the Profile Update screen.
    
    Issue:
    Components (except for one JTextField) disappear once the password change button is clicked.

--- src/main/java/fd/UserInfoFrame.java
+++ src/main/java/fd/UserInfoFrame.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class UserInfoFrame extends JFrame implements ActionListener {
 
@@ -11,7 +13,7 @@ public class UserInfoFrame extends JFrame implements ActionListener {
     private JLabel underLine;
 
     private JTextField firstNameField, lastNameField, emailField, userField;
-    private JPasswordField passField, confirmPassField;
+    private JPasswordField passField;
     private JButton updateButton;
     private JLabel err1, err2, err3, err4;
 
@@ -49,9 +51,74 @@ public class UserInfoFrame extends JFrame implements ActionListener {
         underLine.setBounds(189, 65, 422, 9);
         add(underLine);
 
-        // content
-        // detailsPanel.add(new JLabel("Details content goes here"));
-        // passChangePanel.add(new JLabel("Password change content goes here"));
+        // transferred content
+        JLabel firstNameLabel = new JLabel("First Name");
+        firstNameLabel.setFont(UI.MB15);
+        firstNameLabel.setBounds(189, 98, 422, 25);
+        this.add(firstNameLabel);
+
+        firstNameField = UI.genRoundTextField("", 20, "#FFFFFF", false);
+        firstNameField.setBounds(189, 123, 198, 45);
+        firstNameField.setFont(UI.CB18);
+        this.add(firstNameField);
+
+        JLabel lastNameLabel = new JLabel("Last Name");
+        lastNameLabel.setFont(UI.MB15);
+        lastNameLabel.setBounds(413, 98, 422, 25);
+        this.add(lastNameLabel);
+
+        lastNameField = UI.genRoundTextField("", 20, "#FFFFFF", false);
+        lastNameField.setBounds(413, 123, 198, 45);
+        lastNameField.setFont(UI.CB18);
+        this.add(lastNameField);
+
+        JLabel emailLabel = new JLabel("Email");
+        emailLabel.setFont(UI.MB15);
+        emailLabel.setBounds(189, 185, 422, 25);
+        this.add(emailLabel);
+
+        emailField = UI.genRoundTextField("", 20, "#FFFFFF", false);
+        emailField.setBounds(189, 210, 422, 45);
+        emailField.setFont(UI.CB18);
+        this.add(emailField);
+
+        JLabel signupUsernameLabel = new JLabel("Username");
+        signupUsernameLabel.setFont(UI.MB15);
+        signupUsernameLabel.setBounds(189, 272, 422, 25);
+        this.add(signupUsernameLabel);
+
+        userField = UI.genRoundTextField("", 20, "#FFFFFF", false);
+        userField.setBounds(189, 297, 422, 45);
+        userField.setFont(UI.CB18);
+        this.add(userField);
+
+        JLabel signupPasswordLabel = new JLabel("Password");
+        signupPasswordLabel.setFont(UI.MB15);
+        signupPasswordLabel.setBounds(189, 359, 422, 25);
+        this.add(signupPasswordLabel);
+
+        passField = UI.genRoundPasswordField("", 20, "#FFFFFF", false);
+        passField.setBounds(189, 384, 422, 45);
+        passField.setFont(UI.CB18);
+        this.add(passField);
+
+        err1 = new JLabel("");
+        err1.setFont(UI.MB12);
+        err1.setBounds(620, 124, 130, 40);
+        this.add(err1);
+        err2 = new JLabel("");
+        err2.setFont(UI.MB12);
+        err2.setBounds(620, 211, 150, 40);
+        this.add(err2);
+        err3 = new JLabel("");
+        err3.setFont(UI.MB12);
+        err3.setBounds(620, 298, 150, 40);
+        this.add(err3);
+        err4 = new JLabel("");
+        err4.setFont(UI.MB12);
+        err4.setBounds(620, 385, 150, 40);
+        this.add(err4);
+
 
         // adding panels to cards
         cards.add(detailsPanel, "Details");
