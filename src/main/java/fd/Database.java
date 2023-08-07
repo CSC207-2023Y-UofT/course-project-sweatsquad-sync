148cd3e1474b53b0ae1e4c84c843e8ce1fd45cf4
lachrymaL <lachrymalfutura@gmail.com>
2023-08-07 05:51:59 -0400

    Completed rudimentary ScheduleFrame. At least it works!

--- src/main/java/fd/Database.java
+++ src/main/java/fd/Database.java
@@ -425,4 +425,19 @@ public class Database {
                 }).collect(Collectors.toList());
     }
 
+
+    public String getScheduleBlockAt(int hr, int day) {
+        DayOfWeek[] day_jump = new DayOfWeek[]{ DayOfWeek.SUNDAY,  DayOfWeek.MONDAY,  DayOfWeek.TUESDAY,  DayOfWeek.WEDNESDAY,  DayOfWeek.THURSDAY,  DayOfWeek.FRIDAY,  DayOfWeek.SATURDAY};
+        class R {
+            LocalTime t;
+            DayOfWeek day;
+            Duration d;
+            String name, room;
+            R(LocalTime t, DayOfWeek day, Duration d, String n, String room) { this.t = t; this.day = day; this.d = d; this.name = n; this.room = room; }
+        }
+        return "<HTML>" + activeUser.getWorkouts().stream().flatMap(w -> w.offerings.stream()
+                .map(o -> new R(o.start, o.day, o.duration, w.name, o.room.name)))
+                .filter(r -> r.day == day_jump[day] && r.t.getHour() <= hr && hr < r.t.plus(r.d).getHour())
+                .map(r -> r.name + "<BR>" + r.room).collect(Collectors.joining("<BR>")) + "</HTML>";
+    }
 }
